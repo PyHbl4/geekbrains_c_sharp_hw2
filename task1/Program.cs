36ef6da025094709ab28374678da895fd8dd63e9
PyHbl4 <maxim_runkov@mail.ru>
2022-08-10 01:50:14 +0500

    finish task1

---
+++ task1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int num = number / 10;
+int result = num - ((num / 10) * 10);
+Console.WriteLine("Вторая цифра цисла - " + result);
