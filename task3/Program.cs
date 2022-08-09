2dff2333705d1d8716e68377cd273a78e89d25af
PyHbl4 <maxim_runkov@mail.ru>
2022-08-10 02:28:17 +0500

    finish task3

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Какой порядковый день у сегодняшнего дня недели?");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+if (dayNumber == 6 ^ dayNumber == 7)
+{
+    Console.WriteLine("Поздравляю, сегодня выходной!");
+}
+else
+{
+    Console.WriteLine("Увы, сегодня рабочий день...");
+}
