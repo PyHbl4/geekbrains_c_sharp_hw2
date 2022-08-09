788ebf07456aed4841ea8737e9f8e006026731ec
PyHbl4 <maxim_runkov@mail.ru>
2022-08-10 02:19:49 +0500

    finish task2

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number / 100) < 1)
+{
+    Console.WriteLine("Третья цифра в числе отсутствует");
+}
+else
+{
+    char numberString = number.ToString()[2];
+    Console.WriteLine("Третья цифра в числе: " + numberString);
+}
